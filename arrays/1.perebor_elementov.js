b42481034e4b1c21d9457c71763267c184814652
NatalyaVoronko <vna.natasha@gmail.com>
2021-07-16 00:47:29 +0300

    array homework

---
+++ arrays/1.perebor_elementov.js
@@ -0,0 +1,14 @@
+// 1. Перебор массива с выводом в консоль кажого элемента
+
+const arr = ['Viktor', 'Olga', 'Vitaliy', 'Oleg', 'Svetlana'];
+
+for (const item of arr) {
+    console.log (item);
+}
+
+// variant 2:
+
+// const arr = ['Viktor', 'Olga', 'Vitaliy', 'Oleg', 'Svetlana'];
+// for (let i=0; i<arr.length; i++) {
+//    console.log( arr[i] );
+//}
