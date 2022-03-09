747afce0f99cf1734f267d5f120159828a3f9342
Natallia Varonka <n.varonka@andersenlab.com>
2022-03-09 17:52:54 +0300

    Date https://learn.javascript.ru/date

--- arrays/array-methods_tasks/camelcase.js
+++ arrays/array-methods_tasks/camelcase.js
@@ -11,14 +11,5 @@
 // потом переделайте всё как нужно и методом join соедините обратно.
 
 
-function camelize(string) {
 
-    let string = "my-short-string";
-    let arr = string.split(",");
-
-    for (let i = 0; i < arr.length; i++) {
-        arr[i] = arr[i].toUpperCase();
-    }
-;
-alert(camelize(string));
 
