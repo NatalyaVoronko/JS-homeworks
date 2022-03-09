3d2845d1621c56d2db3fd1dd0ffd1a1e1ee7d1c0
Natallia Varonka <n.varonka@andersenlab.com>
2022-03-09 17:54:47 +0300

    Date https://learn.javascript.ru/date

--- arrays/2.arr_into_string.js
+++ arrays/2.arr_into_string.js
@@ -1,1 +1,9 @@
-zxsd
+// 2. Перобразовать массив в строку и обратно в массив
+
+const fruit = ['orange', 'banana', 'apple', 'pear', 'peach'];
+
+const stringFruit = fruit.join(',');
+console.log (stringFruit);
+
+const arrayFruit = stringFruit.split(',');
+console.log (arrayFruit);
